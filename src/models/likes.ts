b1418952d48c0db4aed2f99d7a3dc9d39b630505
20741901 <RAnKuznetsov@sberbank.ru>
2023-07-18 13:50:34 +0300

    add likes

--- src/models/likes.ts
+++ src/models/likes.ts
@@ -1,38 +1,122 @@
 import {EmployeeStatus} from "../interfaces";
+import Меламед from '../assets/images/Сергей Меламед.jpeg';
+import ЧернышоваЕлена from '../assets/images/Елена Чернышова.jpeg';
+import Юлианна from '../assets/images/Юлианна Егорова.jpeg'
+import ШебановаОльга from "../assets/images/Шебанова Ольга Евгеньевна.jpeg";
+import СеркбаеваАлтын from "../assets/images/Серкбаева Алтын Суннатовна.jpeg";
 
 export const likes = {
     isLiked: true,
     likesAmount: 12,
     bossNames: [
         "Попов А.Л",
-        "Анисимова И.С.",
-        "Константинопольский С.В."
+        "Ситнов В.В.",
+        "Меламед С.В.",
+        "Чернышова Е.В.",
+        "Егорова Ю.И.",
     ],
     people: [{
         position: '1',
-        list: [{
-            employeeNumber: "01924077",
-            photoData: "Base64",
-            terDivisionName: "ЦБ",
-            firstName: "Вероника",
-            lastName: "Трошина",
-            colorCode: {
-                primary: "blue-60",
-                secondary: "blue-30"
-            },
-            employeeStatus: EmployeeStatus.Top_Manager
-        },
+        list: [
             {
-                employeeNumber: "01924077",
-                photoData: "URL_in_Base64",
+                employeeNumber: "40",
+                photoData: '',
                 terDivisionName: "ЦБ",
-                firstName: "Вероника",
-                lastName: "Трошина",
+                firstName: "Анатолий",
+                lastName: "Попов",
+                colorCode: {
+                    primary: "gradient",
+                    secondary: "gradient"
+                },
+                employeeStatus: EmployeeStatus.Top_Manager
+            },
+            {
+                employeeNumber: "41",
+                photoData: "",
+                terDivisionName: "ЦБ",
+                firstName: "Владимир",
+                lastName: "Ситнов",
+                colorCode: {
+                    primary: "gradient",
+                    secondary: "gradient"
+                },
+                employeeStatus: EmployeeStatus.Top_Manager
+            },
+            {
+                employeeNumber: "42",
+                photoData: Меламед,
+                terDivisionName: "ЦБ",
+                firstName: "Сергей",
+                lastName: "Меламед",
+                colorCode: {
+                    primary: "gradient",
+                    secondary: "gradient"
+                },
+                employeeStatus: EmployeeStatus.Top_Manager
+            },
+            {
+                employeeNumber: "43",
+                photoData: ЧернышоваЕлена,
+                terDivisionName: "ЦБ",
+                firstName: "Елена",
+                lastName: "Чернышова",
+                colorCode: {
+                    primary: "gradient",
+                    secondary: "gradient"
+                },
+                employeeStatus: EmployeeStatus.Top_Manager
+            },
+            {
+                employeeNumber: "44",
+                photoData: Юлианна,
+                terDivisionName: "ЦБ",
+                firstName: "Юлианна",
+                lastName: "Егорова",
+                colorCode: {
+                    primary: "gradient",
+                    secondary: "gradient"
+                },
+                employeeStatus: EmployeeStatus.Top_Manager
+            },
+            {
+                employeeNumber: "25",
+                isMarked: false,
+                firstName: "Алтын",
+                lastName: "Серкбаева",
+                photoData: СеркбаеваАлтын,
+                terDivisionName: "УБ",
                 colorCode: {
                     primary: "blue-60",
-                    secondary: "blue-30"
+                    secondary: "blue-30",
                 },
                 employeeStatus: EmployeeStatus.Contestant
-            }]
+            },
+            {
+                employeeNumber: "11",
+                isMarked: false,
+                firstName: "Ярослав",
+                lastName: "Митосев",
+                photoData: "",
+                terDivisionName: "МБ",
+                colorCode: {
+                    primary: "orange-60",
+                    secondary: "orange-30",
+                },
+                employeeStatus: EmployeeStatus.Contestant
+            },
+            {
+                employeeNumber: "31",
+                isMarked: false,
+                firstName: "Ольга",
+                lastName: "Шебанова",
+                photoData: ШебановаОльга,
+                terDivisionName: "ЮЗБ",
+                colorCode: {
+                    primary: "light-green-30",
+                    secondary: "light-green-20",
+                },
+                employeeStatus: EmployeeStatus.Contestant
+            },
+        ]
     }]
 }
