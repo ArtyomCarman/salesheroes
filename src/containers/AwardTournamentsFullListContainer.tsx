88a7796646e2d410ad60da02cd7a71ce4b4b6895
20741901 <RAnKuznetsov@sberbank.ru>
2023-07-18 14:01:32 +0300

    add likes + tournaments

--- src/containers/AwardTournamentsFullListContainer.tsx
+++ src/containers/AwardTournamentsFullListContainer.tsx
@@ -2,51 +2,69 @@ import {Flex, Text} from '@chakra-ui/react';
 
 import {TournamentArchiveCard, TournamentCard,} from '../components';
 import {Reward} from '../interfaces';
+import КузьменкоИгорь from "../assets/images/Кузьменко Игорь Владимирович1.jpeg";
 
 const tournaments = {
-	tournamentName: 'Регата эффективности',
+	tournamentName: 'Марафон по условным сделкам',
 	activityTypes: [
 		{
 			activityTypeName: 'Активные',
 			tournaments: [
 				{
-					tournamentId: 'Т1_06',
+					tournamentId: 'T1',
 					employee: {
-						employeeNumber: '343536',
-						photoData: '',
-						firstName: 'Ильшат',
-						lastName: 'Изотов',
+						employeeNumber: "22",
+						photoData: КузьменкоИгорь,
+						firstName: "Игорь",
+						lastName: "Кузьменко",
 						colorCode: {
-							primary: 'blue-30',
-							secondary: 'blue-60',
+							primary: "blue-60",
+							secondary: "blue-30",
 						},
 					},
 
 					tournamentInfo: 'осталось 2 дня',
 					tournamentDurationName: 'ТУРНИР НЕДЕЛИ',
 					progressBarValue: 70,
-					updateTooltip: 'Данные обновляются каждые 2 недели',
-					reward: Reward.silver,
+					updateTooltip: 'Обновление информации об участниках и их достижениях происходит каждый день. Последнее обновление — 24 июля',
+					reward: Reward.gold,
 					rating: [
 						{
-							divisionLevelResult: 'из 1250 в стране',
+							divisionLevelResult: "из 33 в стране",
 							placeInRating: 12,
+						},
+						{
+							divisionLevelResult: "из 3 в ТБ",
+							placeInRating: 2,
 							reward: {
 								icon: Reward.silver,
-								tooltip: 'Претендент на победу',
+								tooltip: "Претендент на победу",
 							},
 						},
 						{
-							divisionLevelResult: 'из 128 в кластере',
-							placeInRating: 7,
+							divisionLevelResult: "из 1 в ГОСБ",
+							placeInRating: 1,
 							reward: {
 								icon: Reward.gold,
-								tooltip: 'Победитель турнира',
+								tooltip: "Претендент на победу",
 							},
 						},
+					],
+				},
+				{
+					tournamentId: 'T2',
+					employee: undefined,
+					tournamentInfo: "Осталось 4 дня",
+					tournamentDurationName: 'ТУРНИР НЕДЕЛИ',
+					progressBarValue: 50,
+					updateTooltip: 'Обновление информации об участниках и их достижениях происходит каждый день. Последнее обновление — 24 июля',
+					reward: undefined,
+					rating: [
 						{
-							divisionLevelResult: 'из 15 в подразделении',
-							placeInRating: 5,
+							divisionLevelResult: "33 в стране",
+						},
+						{
+							divisionLevelResult: "3 в ТБ",
 						},
 					],
 				},
@@ -56,40 +74,39 @@ const tournaments = {
 			activityTypeName: 'Завершенные',
 			tournaments: [
 				{
-					tournamentId: 'Т1_04',
+					tournamentId: 'T3',
 					employee: {
-						employeeNumber: '343536',
-						photoData: '',
-						firstName: 'Ильшат',
-						lastName: 'Изотов',
+						employeeNumber: "22",
+						photoData: КузьменкоИгорь,
+						firstName: "Игорь",
+						lastName: "Кузьменко",
 						colorCode: {
-							primary: 'blue-30',
-							secondary: 'blue-60',
+							primary: "blue-60",
+							secondary: "blue-30",
 						},
 					},
-					tournamentInfo: 'Турнир недели завершён • 17.02–23.02.23',
-					updateTooltip: 'Данные обновляются каждые 2 недели',
-					reward: Reward.silver,
+					tournamentInfo: 'Турнир недели завершён • 07.03–07.09.23',
+					reward: Reward.gold,
 					rating: [
 						{
-							divisionLevelResult: 'из 1250 в стране',
-							placeInRating: 12,
+							divisionLevelResult: "из 33 в стране",
+							placeInRating: 5,
+						},
+						{
+							divisionLevelResult: "из 3 в стране",
+							placeInRating: 2,
 							reward: {
 								icon: Reward.silver,
-								tooltip: 'Претендент на победу',
+								tooltip: "Победитель турнира",
 							},
 						},
 						{
-							divisionLevelResult: 'из 128 в кластере',
-							placeInRating: 7,
+							divisionLevelResult: "из 1 в стране",
+							placeInRating: 1,
 							reward: {
 								icon: Reward.gold,
-								tooltip: 'Победитель турнира',
+								tooltip: "Победитель турнира",
 							},
-						},
-						{
-							divisionLevelResult: 'из 15 в подразделении',
-							placeInRating: 5,
 						},
 					],
 				},
