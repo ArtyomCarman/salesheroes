f9002d815b02d81de785d7c31d61c1aa6ee14075
Артём Карманов <artyomcarman@gmail.com>
2023-07-18 11:28:38 +0300

    badges 6

--- src/components/InfoBlock.tsx
+++ src/components/InfoBlock.tsx
@@ -1,25 +1,27 @@
-import { FC } from 'react';
-import { Grid, Text } from '@chakra-ui/react';
+import { FC } from "react";
+import { Grid, Text } from "@chakra-ui/react";
 
-import { ReactComponent as Info } from '../assets/icons/info.svg';
+import { ReactComponent as Info } from "../assets/icons/info.svg";
 
 interface Props {
-	text: string;
+  text: string;
 }
 
 export const InfoBlock: FC<Props> = ({ text }) => {
-	return (
-		<Grid
-			gap="16px"
-			overflow="hidden"
-			borderRadius="16px"
-			padding="12px 16px"
-			borderColor="rgba(87, 92, 112, 0.5)"
-			borderWidth="1px"
-			templateColumns="auto 1fr"
-		>
-			<Info width="24px" height="24px" />
-			<Text color="rgba(255, 255, 255, 0.6)">{text}</Text>
-		</Grid>
-	);
+  return (
+    <Grid
+      gap="16px"
+      overflow="hidden"
+      borderRadius="16px"
+      padding="12px 16px"
+      borderColor="rgba(87, 92, 112, 0.5)"
+      borderWidth="1px"
+      templateColumns="auto 1fr"
+    >
+      <Info width="24px" height="24px" />
+      <Text size="caption12/16" color="rgba(255, 255, 255, 0.6)">
+        {text}
+      </Text>
+    </Grid>
+  );
 };
