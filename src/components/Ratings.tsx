f1746eac2aeb5812275854225e6d768ad3da41ef
20741901 <RAnKuznetsov@sberbank.ru>
2023-07-20 11:39:32 +0300

    try to fix pwa

--- src/components/Ratings.tsx
+++ src/components/Ratings.tsx
@@ -1,9 +1,10 @@
 import { FC } from "react";
-import { Link } from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
 
-import {  IDivisions } from "../interfaces";
+import {IDivisions, IProfileBody} from "../interfaces";
 import { Empty } from "./Empty";
+import {leaders} from "../models/tournamen-leaders";
 
 export const Ratings: FC<Props> = ({ divisions, timePeriod, profileId }) => {
   // const navigate = useNavigate();
@@ -19,6 +20,7 @@ export const Ratings: FC<Props> = ({ divisions, timePeriod, profileId }) => {
   //     );
   //   }
   // };
+  const { employeeNumber } = useParams()
 
   if (divisions.length === 0 && timePeriod === "За всё время") {
     return (
