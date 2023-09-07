6958a1519217c9982411ce923d0f98f32f6cd55d
Артём Карманов <artyomcarman@gmail.com>
2023-09-07 14:30:54 +0400

    shop page

--- src/components/ShopCard.tsx
+++ src/components/ShopCard.tsx
@@ -1,15 +1,25 @@
-import { Box, Text, Grid } from "@chakra-ui/react";
+import { Box, Text, Grid, Flex } from "@chakra-ui/react";
 import { FC } from "react";
 import { ButtonComponent } from "./ButtonComponent";
+import ShopTreePng from "../assets/images/shop-tree.png";
+import { ReactComponent as DiamondIcon } from "../assets/icons/diamond.svg";
 
 export const ShopCard: FC<Props> = ({ card, canBuy }) => {
   return (
-    <Box borderRadius="16px" bgColor="#1A1C22" p="24px">
+    <Box
+      borderRadius="16px"
+      bgColor="#1A1C22"
+      p="24px"
+      bg={`url(${ShopTreePng})`}
+      bgSize="cover"
+      backdropFilter="opacity(.54)"
+    >
       <Text mb="16px">{card.title}</Text>
       <Grid gap="24px">
-        <Box>
+        <Flex gap="8px">
           <Text>{card.price}</Text>
-        </Box>
+          <DiamondIcon width="24px" height="24px" />
+        </Flex>
         {canBuy && (
           <ButtonComponent
             text="Приобрести"
