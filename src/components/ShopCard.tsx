a658551a95b6383efe5c30cc9b842b0ad7d0e565
Артём Карманов <artyomcarman@gmail.com>
2023-09-07 15:19:35 +0400

    shop card

--- src/components/ShopCard.tsx
+++ src/components/ShopCard.tsx
@@ -8,26 +8,30 @@ export const ShopCard: FC<Props> = ({ card, canBuy }) => {
   return (
     <Box
       borderRadius="16px"
+      overflow="hidden"
       bgColor="#1A1C22"
-      p="24px"
       bg={`url(${ShopTreePng})`}
       bgSize="cover"
-      backdropFilter="opacity(.54)"
     >
-      <Text mb="16px">{card.title}</Text>
-      <Grid gap="24px">
-        <Flex gap="8px">
-          <Text>{card.price}</Text>
-          <DiamondIcon width="24px" height="24px" />
-        </Flex>
-        {canBuy && (
-          <ButtonComponent
-            text="Приобрести"
-            onClick={() => {}}
-            w="min-content"
-          />
-        )}
-      </Grid>
+      <Box
+        p="24px"
+        bg="linear-gradient(0deg, rgba(91, 67, 6, 0.54) 0%, rgba(0, 0, 0, 0.00) 74.47%), linear-gradient(0deg, rgba(0, 0, 0, 0.29) 0%, rgba(0, 0, 0, 0.29) 100%), linear-gradient(270deg, rgba(104, 11, 78, 0.64) 0%, rgba(6, 14, 84, 0.72) 78.65%)"
+      >
+        <Text mb="16px">{card.title}</Text>
+        <Grid gap="24px">
+          <Flex gap="8px">
+            <Text>{card.price}</Text>
+            <DiamondIcon width="24px" height="24px" />
+          </Flex>
+          {canBuy && (
+            <ButtonComponent
+              text="Приобрести"
+              onClick={() => {}}
+              w="min-content"
+            />
+          )}
+        </Grid>
+      </Box>
     </Box>
   );
 };
