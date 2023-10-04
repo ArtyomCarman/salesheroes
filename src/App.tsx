0101267fa3de148cf0b04684215e01f65190bd9a
Wenn911 <wenn123@yandex.ru>
2023-10-04 17:50:59 +0300

    add password

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { Button, ChakraProvider, FormControl, Input } from "@chakra-ui/react";
 import { CacheProvider } from "@emotion/react";
 import createCache from "@emotion/cache";
 import { AppRoutes } from "./AppRoutes";
 import { theme } from "./utils";
+import { useState } from "react";
 
 const emotionCache = createCache({
   key: "emotion-css-cache",
@@ -10,10 +11,38 @@ const emotionCache = createCache({
 });
 
 const App = () => {
+  const [isVerified, setIsVerified] = useState(false);
+
+  const checkPw = () => {
+    const answer = (document.getElementById("password") as HTMLInputElement)
+      ?.value;
+
+    if (answer === "q12345678") {
+      setIsVerified(true);
+    } else {
+      alert("Неверный пароль");
+    }
+  };
+
   return (
     <CacheProvider value={emotionCache}>
       <ChakraProvider theme={theme}>
-        <AppRoutes />
+        {isVerified ? (
+          <AppRoutes />
+        ) : (
+          <form onSubmit={checkPw}>
+            <Input
+              placeholder="Введите пароль"
+              type="password"
+              id="password"
+              name="password"
+              width="300px"
+            />
+            <Button width="300px" type="submit">
+              Войти
+            </Button>
+          </form>
+        )}
       </ChakraProvider>
     </CacheProvider>
   );
